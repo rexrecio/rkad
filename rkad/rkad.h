39de51fda56d354d7084767196ec4b8ac3be24dc
Rex Recio <rjr@hp-841r5165ob0e.apac.sykes.com>
2014-11-25 15:44:03 +0800

    Initial Commit

---
+++ rkad/rkad.h
@@ -0,0 +1,19 @@
+//
+//  rkad.h
+//  rkad
+//
+//  Created by Rex Recio on 11/25/14.
+//  Copyright (c) 2014 Rex Recio. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for rkad.
+FOUNDATION_EXPORT double rkadVersionNumber;
+
+//! Project version string for rkad.
+FOUNDATION_EXPORT const unsigned char rkadVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <rkad/PublicHeader.h>
+
+
